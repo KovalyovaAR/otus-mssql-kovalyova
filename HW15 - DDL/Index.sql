0ecac5a1ef8915940a130eb119a37f9f36c764d8
KovalyovaAR <alekssandra.rover@gmail.com>
2025-03-24 19:51:57 +0300

    'HW15'

---
+++ HW15 - DDL/Index.sql
@@ -0,0 +1,13 @@
+USE [project]
+GO
+
+/****** Object:  Index [IX_Trips_EmpID_Dates]    Script Date: 24.03.2025 19:47:20 ******/
+CREATE NONCLUSTERED INDEX [IX_Trips_EmpID_Dates] ON [dbo].[Trips]
+(
+	[EmployeeID] ASC,
+	[StartDate] ASC,
+	[EndDate] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+
+
